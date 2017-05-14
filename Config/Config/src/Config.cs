69c772cde21c1126af1cd9d3e5c0d2f9e220fd07
SZSilence06 <342631359@qq.com>
2017-05-14 22:56:03 +0800

    Add new project for Config.

---
+++ Config/Config/src/Config.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Config
+{
+    internal class Configuration
+    {
+        private enum LightShadowLevel
+        {
+            NONE,
+            NORMAL,
+            HIGH
+        };
+
+        private bool useMultiSample = true;
+        private bool useVerticalAsync = true;
+        private LightShadowLevel lightShadowLevel = LightShadowLevel.HIGH;
+
+        public Configuration()
+        {
+        }
+
+        public void Load()
+        {
+        }
+    }
+}
